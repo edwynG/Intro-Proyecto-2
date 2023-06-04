8928cab8ca33ebf451d8d837864eb95e96b2d661
Edwyn Guzman <edwynjesus30@gmail.com>
2023-06-04 02:07:47 -0400

    Layout-estudiante_50%

---
+++ JS/monitor.js
@@ -0,0 +1,15 @@
+
+
+
+
+//abrir nav resposive
+document.querySelector(".header_menu-resposive").addEventListener("click", ()=>{
+    document.querySelector(".nav_lista").classList.toggle("responsive-lista");
+    document.querySelector(".container_main").classList.toggle("responsive-lista");
+})
+
+//cerrar nav en reposive
+document.querySelector(".nav_reposive-icoClose").addEventListener("click",()=>{
+    document.querySelector(".nav_lista").classList.toggle("responsive-lista");
+    document.querySelector(".container_main").classList.toggle("responsive-lista");
+})
